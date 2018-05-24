9b01c15185d0a480bc740b679321fae5ad70ffe2
RobertoPanoi <roberto@panoimagen.com>
2018-05-24 17:10:42 +0200

    Compatibility changes for Qgis 3.0.
    Compat QgsGeometry.fromPolylineXY QGis3 - QgsGeometry.fromPolyline QGIS2.

--- apicompat/qgsgeometry.py
+++ apicompat/qgsgeometry.py
@@ -2,3 +2,4 @@ import qgis.core
 
 # fromPoint() was renamed to fromPointXY() in QGis3
 qgis.core.QgsGeometry.fromPointXY = qgis.core.QgsGeometry.fromPoint
+qgis.core.QgsGeometry.fromPolylineXY = qgis.core.QgsGeometry.fromPolyline
